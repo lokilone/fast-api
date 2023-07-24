b8135c5e02b136340a86046490edeabdc0a23793
OlivierA59000 <oamable@gmail.com>
2023-07-24 14:21:36 +0200

    update

--- app/main.py
+++ app/main.py
@@ -3,51 +3,41 @@ from fastapi import Header
 from pydantic import BaseModel
 from typing import Optional, List
 
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from passlib.context import CryptContext
+
 app = FastAPI()
+security = HTTPBasic()
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-# users_db
-
-users_db ={
+users ={
     "alice": "wonderland",
     "bob": "builder",
     "clementine": "mandarine"
     }
 
+def get_current_user(credentials: HTTPBasicCredentials = Depends(security)):
+    username = credentials.username
+    if not(users.get(username)) or not(pwd_context.verify(credentials.password, users[username])):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/user")
+def current_user(username: str = Depends(get_current_user)):
+    return "Hello {}".format(username)
+
 @app.get('/users')
 def get_users():
-    return users_db
+    return users.keys
 # curl -X GET -i http://127.0.0.1:8000/users
 
 @app.get('/users/{username}')
 def get_user(username):
-    password = users_db.get(username)
+    password = users.get(username)
     return {username: password}
 # curl -X GET -i http://127.0.0.1:8000/users/alice
-
-# Créer utilisateur
-@app.put('/users')
-def put_users(username, password):
-    users_db[username]=password
-    return {username: password}
-
-# Modifier utilisateur
-@app.post('/users/{username}')
-def post_user(username, password):
-    try:
-        users_db[username]=password
-        return {username: password}
-    except IndexError:
-        return {}
-
-
-# supprimer utilisateur
-@app.delete('/users/{username}')
-def delete_user(username):
-    try:
-        del users_db[username]
-        return {
-            'deleted': True
-            }
-    except IndexError:
-        return {}
-
